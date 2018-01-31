a1f043080c8a3498602ca698b8cfc3313096c064
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-01-31 18:52:30 +0100

    Hook up Frontend With Backend REST API

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask, render_template, jsonify
+import flask
+import flask_sqlalchemy
+import flask_restless
 import requests
 
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-class VueFlask(Flask):
+class VueFlask(flask.Flask):
     @property
     def static_folder(self):
         if self.debug:
@@ -15,14 +17,38 @@ class VueFlask(Flask):
             return os.path.join(ROOT_PATH, 'static')
 
 
-app = VueFlask(__name__,
-               static_folder=None,
-               template_folder=os.path.join(ROOT_PATH, 'frontend'))
+db = flask_sqlalchemy.SQLAlchemy()
 
+
+class Alarm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String, nullable=False, default='00:00')
+    enabled = db.Column(db.Boolean, nullable=False, default=True)
+    repeat = db.Column(db.Boolean, nullable=False, default=False)
+    repeatDays = db.Column(db.Integer, nullable=False, default=0)
+
+
+def create_app():
+    app = VueFlask(__name__,
+                   static_folder=None,
+                   template_folder=os.path.join(ROOT_PATH, 'frontend'))
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+    manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+    manager.create_api(Alarm,
+                       url_prefix='/api/1.0',
+                       methods=['GET', 'POST', 'PATCH', 'DELETE'])
+    return app
+
+
+app = create_app()
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     if app.debug:
         return requests.get('http://localhost:8080/{}'.format(path)).text
-    return render_template("index.html")
+    return flask.render_template("index.html")
