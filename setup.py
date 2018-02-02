b1004b73ca93b9e036ae6ca38dffebb81f8efd69
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-02-02 18:45:37 +0100

    Add Python Package Files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rp-sunrise-alarm',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'attrs',
+        'Flask',
+        'Flask-SQLAlchemy',
+        'requests',
+    ]
+)
