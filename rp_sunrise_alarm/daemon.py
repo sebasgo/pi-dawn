7f8bd19e5596a8813d5ed3a13e7edb058c374729
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-02-06 22:39:44 +0100

    Reschedule Alarms When They Are in the Past
    One time alarms are disabled and repeated are rescheduled.

--- rp_sunrise_alarm/daemon.py
+++ rp_sunrise_alarm/daemon.py
@@ -44,6 +44,19 @@ def configure_led_screen(state: comm.State, alarms, led_screen):
     led_screen.draw_surface(surface)
 
 
+def reschedule_alarms(alarms):
+    dirty = False
+    cutoff = datetime.datetime.now() - datetime.timedelta(seconds=app.config['ALARM_POST_DURATION'])
+    for alarm in alarms:
+        if alarm.next_alarm is not None and alarm.next_alarm < cutoff:
+            dirty = True
+            if not alarm.repeat:
+                alarm.enabled = False
+            alarm.schedule_next_alarm()
+    if dirty:
+        model.db.session.commit()
+
+
 def find_active_alarm(alarms):
     now = datetime.datetime.now()
     for alarm in alarms:
@@ -70,8 +83,7 @@ while True:
         model.db.session.rollback()
         alarms = model.Alarm.query.order_by(model.Alarm.time).all()
     configure_led_screen(state, alarms, led_screen)
-    print(state)
-
+    reschedule_alarms(alarms)
     comm.set_state(app, state)
 
 
