442e99a0f68e159aea8ab76601dafed80712cc6c
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-03-17 18:12:54 +0100

    Extend Metadata in `setup.py`

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from subprocess import check_call
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 
+base_dir = os.path.dirname(__file__)
+
 
 class BuildCommand(build_py):
 
     def run(self):
-        base_dir = os.path.dirname(__file__)
         cmd_dir = os.path.join(base_dir, 'rp_sunrise_alarm', 'frontend')
         check_call(['npm', 'install'], cwd=cmd_dir)
         check_call(['npm', 'run', 'build'], cwd=cmd_dir)
@@ -17,6 +18,11 @@ class BuildCommand(build_py):
 setup(
     name='rp-sunrise-alarm',
     version='1.0',
+    description='Raspberry Pi Sunrise Alarm',
+    long_description=open(os.path.join(base_dir, 'README.rst')).read(),
+    url='https://github.com/sebasgo/rp-sunrise-alarm',
+    author='Sebastian Gottfried',
+    author_email='sebastian.gottfried@posteo.de',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -42,5 +48,14 @@ setup(
     },
     cmdclass={
         'build_py': BuildCommand
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask,'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
