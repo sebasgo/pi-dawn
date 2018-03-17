9478f3fb5cfbeca1b0e356c4d948e9664a10226b
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-03-17 17:32:38 +0100

    Trigger Frontend Build in `setup.py build`

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import os
+
+from subprocess import check_call
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+
+class BuildCommand(build_py):
+
+    def run(self):
+        base_dir = os.path.dirname(__file__)
+        cmd_dir = os.path.join(base_dir, 'rp_sunrise_alarm', 'frontend')
+        check_call(['npm', 'install'], cwd=cmd_dir)
+        check_call(['npm', 'run', 'build'], cwd=cmd_dir)
+        super().run()
 
 setup(
     name='rp-sunrise-alarm',
@@ -26,4 +40,7 @@ setup(
             'rp-sunrise-alarm-daemom=rp_sunrise_alarm.daemon:main',
         ],
     },
+    cmdclass={
+        'build_py': BuildCommand
+    }
 )
