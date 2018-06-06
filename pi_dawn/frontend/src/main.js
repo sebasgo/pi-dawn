891630dc0b6e37e325845cb01d81d30d6b5d4a50
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-06-06 08:42:03 +0200

    Only Import the Required Vuetify Components
    This reduces the size of the compiled JS bundle by about 25%.

--- pi_dawn/frontend/src/main.js
+++ pi_dawn/frontend/src/main.js
@@ -2,10 +2,54 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.css'
 
-Vue.use(Vuetify)
+import {
+  Vuetify,
+  VApp,
+  VAvatar,
+  VBottomNav,
+  VBtn,
+  VBtnToggle,
+  VCard,
+  VCheckbox,
+  VDialog,
+  VFooter,
+  VGrid,
+  VIcon,
+  VSwitch,
+  VTimePicker,
+  VToolbar,
+  transitions
+} from 'vuetify'
+
+import {
+  Ripple
+} from 'vuetify/es5/directives'
+
+require('vuetify/src/stylus/app.styl')
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VAvatar,
+    VBottomNav,
+    VBtn,
+    VBtnToggle,
+    VCard,
+    VCheckbox,
+    VDialog,
+    VFooter,
+    VGrid,
+    VIcon,
+    VSwitch,
+    VTimePicker,
+    VToolbar,
+    transitions
+  },
+  directives: {
+    Ripple
+  }
+})
 
 new Vue({
   el: '#app',
