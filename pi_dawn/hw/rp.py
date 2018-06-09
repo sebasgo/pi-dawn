777513625bd7ef53bb9387d560bc3b8384da9e2d
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2018-06-09 11:54:45 +0200

    Implement Ordered Dithering

--- pi_dawn/hw/rp.py
+++ pi_dawn/hw/rp.py
@@ -17,10 +17,11 @@ class LedScreen:
     def __init__(self, width, height, gamma_r=2, gamma_g=2, gamma_b=2):
         self.width = width
         self.height = height
-        self.pixels = Adafruit_WS2801.WS2801Pixels(width*height, spi=Adafruit_GPIO.SPI.SpiDev(SPI_PORT, SPI_DEVICE))
         self.lut_r = self.build_gamma_lut(gamma_r)
         self.lut_g = self.build_gamma_lut(gamma_g)
         self.lut_b = self.build_gamma_lut(gamma_b)
+        self.bayer_map = self.build_bayer_map()
+        self.pixels = Adafruit_WS2801.WS2801Pixels(width*height, spi=Adafruit_GPIO.SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
     def make_surface(self):
         return graphics.Surface(self)
@@ -30,6 +31,10 @@ class LedScreen:
             for y in range(self.height):
                 r, g, b = surface.get_pixel(x, y)
                 r, g, b = self.lut_r[r], self.lut_g[g], self.lut_b[b]
+                t = self.bayer_map[y % 2][x % 2]
+                r = max(0, min(255, round(r + t)))
+                g = max(0, min(255, round(g + t)))
+                b = max(0, min(255, round(b + t)))
                 if x % 2 == 0:
                     offset = self.height - y - 1 + x * self.height
                 else:
@@ -40,4 +45,15 @@ class LedScreen:
     @staticmethod
     def build_gamma_lut(g):
         inverse_g = 1 / g
-        return [int(255 * ((i / 255) ** inverse_g)) for i in range(256)]
+        return [255 * ((i / 255) ** inverse_g) for i in range(256)]
+
+    @staticmethod
+    def build_bayer_map():
+        map = [
+            [0.0, 2.0],
+            [3.0, 1.0],
+        ]
+        for x in range(2):
+            for y in range(2):
+                map[y][x] = 0.5 * map[y][x] - 1.0
+        return map
