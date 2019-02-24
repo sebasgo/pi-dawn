c308517bcd0b86986f1789285eed7f4127e4dff6
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2019-02-24 19:28:01 +0100

    Fix Classifiers

--- setup.py
+++ setup.py
@@ -61,11 +61,12 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
-        'Framework :: Flask,'
+        'Framework :: Flask',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
