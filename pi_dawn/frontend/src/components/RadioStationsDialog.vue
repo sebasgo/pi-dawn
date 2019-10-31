182a5f97c0d5caa03c7084e89f84edd961749065
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2019-10-31 11:41:58 -0700

    Add a FAB for Station List Dialog
    It will allow to add new stations either via search or manual input of
    the station details.

--- pi_dawn/frontend/src/components/RadioStationsDialog.vue
+++ pi_dawn/frontend/src/components/RadioStationsDialog.vue
@@ -1,12 +1,39 @@
 <template>
-  <v-dialog v-model="dialog" fullscreen hide-overlay>
+  <v-dialog :value="true" fullscreen hide-overlay>
     <v-card tile>
+
       <v-app-bar color="primary">
         <v-btn v-on:click="goBack()" icon>
           <v-icon>mdi-arrow-left</v-icon>
         </v-btn>
         <v-toolbar-title>Radio Stations</v-toolbar-title>
       </v-app-bar>
+
+      <v-speed-dial v-model="fab" bottom right fixed direction="top" transition="fade">
+        <template v-slot:activator>
+          <v-btn v-model="fab" dark fab color="primary">
+            <v-icon v-if="fab">mdi-close</v-icon>
+            <v-icon v-else>mdi-plus</v-icon>
+          </v-btn>
+        </template>
+        <v-tooltip left :value="true">
+          <template v-slot:activator="{ on }">
+            <v-btn dark small fab primary v-on="on">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+          <span>Add manually</span>
+        </v-tooltip>
+        <v-tooltip left :value="true">
+          <template v-slot:activator="{ on }">
+            <v-btn dark small fab primary v-on="on">
+              <v-icon>mdi-search-web</v-icon>
+            </v-btn>
+          </template>
+          <span>Search online</span>
+        </v-tooltip>
+      </v-speed-dial>
+
     </v-card>
   </v-dialog>
 </template>
@@ -15,7 +42,7 @@
   export default {
       data () {
           return {
-              dialog: true
+              fab: false
           }
       },
       methods: {
